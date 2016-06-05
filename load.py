315ff7439c062803ed42133f84c4f7cc186e7cae
liuchen11 <liuchen112013@gmail.com>
2016-06-05 20:36:34 +0200

    loader

--- load.py
+++ load.py
@@ -1,27 +1,28 @@
 import pandas as pd
-"""
-load data
-"""
 
-feature_dict = {i:label for i,label in zip(
-                range(4),
-                  ('sepal length in cm',
-                  'sepal width in cm',
-                  'petal length in cm',
-                  'petal width in cm', ))}
+class loader(object):
 
-df = pd.io.parsers.read_csv(
-    filepath_or_buffer='ionosphere/ionosphere.data',
-    header=None,
-    sep=',',
-    )
+	def __init__(self, file_name, dimension, split_token = ','):
+		self.file_name = file_name
+		self.split_token = split_token
+		self.dimension = dimension
 
-df.columns = range(34) + ['class label']
-df.dropna(how="all", inplace=True) # to drop the empty line at file-end
-df.tail()
+	def load(self):
+		df = pd.io.parsers.read_csv(
+			filepath_or_buffer = self.file_name,
+			header = None,
+			sep = self.split_token)
+		df.columns = range(self.dimension) + ['label']
+		df.dropna(how = 'all', inplace = True) # to drop the empty line at file-end
+		df.tail()
 
-X = df[range(34)].values
-y = df['class label'].values
+		self.X = df[range(self.dimension)].values
+		self.Y = df['label'].values
+		distinct_label = list(set(self.Y))
 
-print X[1,:]
-print y[1:20]
+		if len(distinct_label) != 2:
+			raise Exeception('Two Labels Required\n','Label Sets:\n',distinct_label)
+
+		self.Y = map(lambda x: {distinct_label[0]:1, distinct_label[1]:-1}[x], self.Y)
+
+		return [self.X, self.Y]
