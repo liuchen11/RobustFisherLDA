564c2cb22c433383ae35618aa21d01f518eb478c
Jun Lu <locky1218@gmail.com>
2016-06-05 21:16:11 +0200

    update load class and load log function

--- load.py
+++ load.py
@@ -2,27 +2,33 @@ import pandas as pd
 
 class loader(object):
 
-	def __init__(self, file_name, dimension, split_token = ','):
-		self.file_name = file_name
-		self.split_token = split_token
-		self.dimension = dimension
+    def __init__(self, file_name, split_token = ','):
+        self.file_name = file_name
+        self.split_token = split_token
 
-	def load(self):
-		df = pd.io.parsers.read_csv(
-			filepath_or_buffer = self.file_name,
-			header = None,
-			sep = self.split_token)
-		df.columns = range(self.dimension) + ['label']
-		df.dropna(how = 'all', inplace = True) # to drop the empty line at file-end
-		df.tail()
+    def load(self):
+        df = pd.io.parsers.read_csv(
+        filepath_or_buffer = self.file_name,
+                            header = None,
+                            sep = self.split_token)
 
-		self.X = df[range(self.dimension)].values
-		self.Y = df['label'].values
-		distinct_label = list(set(self.Y))
+        self.dimension = df.shape[1]-1
+        df.columns = range(self.dimension) + ['label']
+        df.dropna(how = 'all', inplace = True) # to drop the empty line at file-end
+        df.tail()
 
-		if len(distinct_label) != 2:
-			raise Exeception('Two Labels Required\n','Label Sets:\n',distinct_label)
+        self.X = df[range(self.dimension)].values
+        self.Y = df['label'].values
+        distinct_label = list(set(self.Y))
 
-		self.Y = map(lambda x: {distinct_label[0]:1, distinct_label[1]:-1}[x], self.Y)
+        if len(distinct_label) != 2:
+            raise Exception('Two Labels Required\n','Label Sets:\n',distinct_label)
 
-		return [self.X, self.Y]
+        self.Y = map(lambda x: {distinct_label[0]:1, distinct_label[1]:-1}[x], self.Y)
+
+        return [self.X, self.Y]
+
+if __name__ == "__main__":
+    loader = loader('ionosphere/ionosphere.data')
+    [X, y] = loader.load()
+    print X.shape
